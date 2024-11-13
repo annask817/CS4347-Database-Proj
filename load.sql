a916f5ae29d1e6a262952b0c130b455d5565d502
AnnaSK <144516813+annask817@users.noreply.github.com>
2024-11-13 14:12:36 -0600

    corrected uid to usid in load.sql

--- load.sql
+++ load.sql
@@ -1,10 +1,10 @@
-LOAD DATA INFILE 'data/user.csv'
+LOAD DATA INFILE 'user.csv'
 INTO TABLE User
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'data/expense.csv'
+LOAD DATA INFILE 'expense.csv'
 INTO TABLE Expense
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -12,7 +12,7 @@ IGNORE 1 LINES
 (expense_id, @purpose)
 SET purpose = REPLACE(@purpose, '\r', '');
 
-LOAD DATA INFILE 'data/income.csv'
+LOAD DATA INFILE 'income.csv'
 INTO TABLE Income
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -20,15 +20,15 @@ IGNORE 1 LINES
 (income_id, @source)
 SET source = REPLACE(@source, '\r', '');
 
-LOAD DATA INFILE 'data/transactions.csv'
+LOAD DATA INFILE 'transactions.csv'
 INTO TABLE Transactions
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(uid, transaction_id, dateOf, @amount)
+(usid, transaction_id, dateOf, @amount)
 SET amount = CAST(REPLACE(@amount, '\r', '') AS SIGNED);
 
-LOAD DATA INFILE 'data/transaction_expense.csv'
+LOAD DATA INFILE 'transaction_expense.csv'
 INTO TABLE Transaction_Expense
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -36,7 +36,7 @@ IGNORE 1 LINES
 (trid, @exid_1)
 SET exid = CAST(REPLACE(@exid_1, '\r', '') AS SIGNED);
 
-LOAD DATA INFILE 'data/transaction_income.csv'
+LOAD DATA INFILE 'transaction_income.csv'
 INTO TABLE Transaction_Income
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -44,7 +44,7 @@ IGNORE 1 LINES
 (trid, @inid_1)
 SET inid = CAST(REPLACE(@inid_1, '\r', '') AS SIGNED);
 
-LOAD DATA INFILE 'data/category.csv'
+LOAD DATA INFILE 'category.csv'
 INTO TABLE Category
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -52,7 +52,7 @@ IGNORE 1 LINES
 (category_id, @category_name)
 SET category_name = REPLACE(@category_name, '\r', '');
 
-LOAD DATA INFILE 'data/expense_category.csv'
+LOAD DATA INFILE 'expense_category.csv'
 INTO TABLE Expense_Category
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -60,7 +60,7 @@ IGNORE 1 LINES
 (catid, @exid_2)
 SET exid = CAST(REPLACE(@exid_2, '\r', '') AS SIGNED);
 
-LOAD DATA INFILE 'data/income_category.csv'
+LOAD DATA INFILE 'income_category.csv'
 INTO TABLE Income_Category
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -68,10 +68,10 @@ IGNORE 1 LINES
 (catid, @inid_2)
 SET inid = CAST(REPLACE(@inid_2, '\r', '') AS SIGNED);
 
-LOAD DATA INFILE 'data/performs.csv'
+LOAD DATA INFILE 'performs.csv'
 INTO TABLE Performs
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(uid, @trid)
+(usid, @trid)
 SET trid = CAST(REPLACE(@trid, '\r', '') AS SIGNED);
